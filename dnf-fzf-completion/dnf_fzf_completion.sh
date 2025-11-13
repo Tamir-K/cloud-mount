85642dd57e1c3ab37e961c1d6bbe9d1fe0f5611e
Tamir K. <121674805+Tamir-K@users.noreply.github.com>
2025-11-13 02:37:35 +0200

    Fix argument passing in fzf completion for dnf

--- dnf-fzf-completion/dnf_fzf_completion.sh
+++ dnf-fzf-completion/dnf_fzf_completion.sh
@@ -1,17 +1,17 @@
 _fzf_complete_dnf() {
 	case "$@" in
 		"dnf ** dnf")
-			_fzf_complete --multi --reverse --prompt="Choose command> " -- "$@" --preview="dnf info {}" --preview-window=67% < <(
+			_fzf_complete --multi --reverse --prompt="Choose command> " --preview="dnf info {}" --preview-window=67% -- "$@" < <(
 				printf "%s\n" install remove update
 			)
 			;;
 		"dnf ** in" | "dnf ** install")
-			_fzf_complete --multi --reverse --prompt="Install packages> " -- "$@" --preview="dnf info {}" --preview-window=67% < <(
+			_fzf_complete --multi --reverse --prompt="Install packages> " --preview="dnf info {}" --preview-window=67% -- "$@" < <(
 				dnf repoquery --queryformat="%{name}\n"
 			)
 			;;
 		"dnf ** rm" | "dnf ** remove")
-			_fzf_complete --multi --reverse --prompt="Remove packages> " -- "$@" --preview="dnf info {}" --preview-window=67% < <(
+			_fzf_complete --multi --reverse --prompt="Remove packages> " --preview="dnf info {}" --preview-window=67% -- "$@" < <(
 				dnf repoquery --installed --queryformat="%{name}\n"
 			)
 			;;
