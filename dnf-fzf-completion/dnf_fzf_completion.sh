ac7aa96db02630bcf20432b3849f68859eaef1c9
Tamir-K <121674805+Tamir-K@users.noreply.github.com>
2025-11-13 02:26:58 +0200

    Add dnf interactive complete with fzf

---
+++ dnf-fzf-completion/dnf_fzf_completion.sh
@@ -0,0 +1,23 @@
+_fzf_complete_dnf() {
+	case "$@" in
+		"dnf ** dnf")
+			_fzf_complete --multi --reverse --prompt="Choose command> " -- "$@" --preview="dnf info {}" --preview-window=67% < <(
+				printf "%s\n" install remove update
+			)
+			;;
+		"dnf ** in" | "dnf ** install")
+			_fzf_complete --multi --reverse --prompt="Install packages> " -- "$@" --preview="dnf info {}" --preview-window=67% < <(
+				dnf repoquery --queryformat="%{name}\n"
+			)
+			;;
+		"dnf ** rm" | "dnf ** remove")
+			_fzf_complete --multi --reverse --prompt="Remove packages> " -- "$@" --preview="dnf info {}" --preview-window=67% < <(
+				dnf repoquery --installed --queryformat="%{name}\n"
+			)
+			;;
+	esac
+}
+
+complete -F _fzf_complete_dnf -o default -o bashdefault dnf
+
+
